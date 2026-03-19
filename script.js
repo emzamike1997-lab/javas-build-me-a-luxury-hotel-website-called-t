c5e056f04f067a3a9baf03d0404c1126d52b4f93
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-19 09:23:51 +0100

    JAVAS: Add script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Add event listener to book now button
 document.querySelector("button").addEventListener("click", function() {
-    alert("Book now button clicked");
+    alert("Booking form will be implemented soon");
 });
 
 // Add event listener to navigation links
 document.querySelectorAll("nav a").forEach(function(link) {
     link.addEventListener("click", function(event) {
         event.preventDefault();
-        // Add logic to navigate to corresponding section
+        // Add smooth scrolling effect
+        document.querySelector(link.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
     });
 });
